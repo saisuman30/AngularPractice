b534d34a2d5bdadfeaa7cb2ae2aa5bb3e96fc43f
Saisuman Yember <saisuman.yember_hcl@kantar.com>
2019-10-30 21:10:43 +0530

    Added Attribute Binding,Class Binding

--- kv-angularcli-componentdemo/src/app/employee/employee.component.ts
+++ kv-angularcli-componentdemo/src/app/employee/employee.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
     selector:'my-employee',
-    templateUrl:'./employee.component.html'
+    templateUrl:'./employee.component.html',
+    styleUrls:['./employee.component.css']
 })
 
 export class EmployeeComponent{
@@ -12,9 +13,23 @@ export class EmployeeComponent{
     age:number=29;
     imagePath:string='https://media.licdn.com/dms/image/C5103AQGzbxX1_aPJBA/profile-displayphoto-shrink_200_200/0?e=1577923200&v=beta&t=fDr3Lm-jW8QYrAzSi_1-kqkh5G9gbdy5bhWcQ1UgSFc';
     isDisabled:boolean=false;
+    EmployeepageDetails:string='These are employee details';
+    colspan:number=2;
+    classesToApply:string="ItalicClass BoldClass";
+    applyBoldClass:boolean=true;
+    applyItalicClass: boolean=false;
+    isBold:boolean=true;
     getFullName():string{
         return this.firstName+this.lastName;
     }   
+
+    AddClasses(){
+        let classes ={
+              boldClass:this.applyBoldClass,
+              ItalicClass:this.applyItalicClass
+        };
+        return classes;
+    }
 
 
 }

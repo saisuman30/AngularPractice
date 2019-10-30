2bafc9b13e2eede1312e5e390a33b669e59a0157
Saisuman Yember <saisuman.yember_hcl@kantar.com>
2019-10-30 13:19:31 +0530

    Added template url and css to angualr component

--- kv-angularcli-componentdemo/src/app/app.component.ts
+++ kv-angularcli-componentdemo/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector:'my-app',
+  templateUrl:'/app.component.html',
+  styleUrls:['/app.component.css']
 })
-export class AppComponent {
-  title = 'kv-angularcli-componentdemo';
+
+export class AppComponent{
+  name:string='Angular';
 }

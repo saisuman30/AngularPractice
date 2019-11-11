cfecc0022a0c291daa0a5b77f7fd25277519ecd0
Saisuman Yember <saisuman.yember_hcl@kantar.com>
2019-11-11 15:23:01 +0530

    Made input and output properties for intercomponent communication.

--- kv-angularcli-componentdemo/src/app/employee/employeeCount.component.ts
+++ kv-angularcli-componentdemo/src/app/employee/employeeCount.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component,Input,Output,EventEmitter} from '@angular/core';
 
 @Component({
     selector:'my-employeecount',
@@ -7,7 +7,23 @@ import {Component} from '@angular/core';
 })
 
 export class EmployeeCountComponent{
-    all:number=5;
-    male:number=3;
-    female:number=2;
+    @Input()
+    all:number;
+    @Input()
+    male:number;
+    @Input()
+    female:number;
+
+    selectedRadioButtonValue:string='All';
+
+    @Output()
+    countRadioButtonSelectedChanged:EventEmitter<string>= new EventEmitter<string>();  
+
+    onSelectedRadioButtonChange(){
+        this.countRadioButtonSelectedChanged.emit(this.selectedRadioButtonValue);
+        console.log(this.selectedRadioButtonValue);
+    }
+
+
+
 }
--- kv-angularcli-componentdemo/src/app/employee/employeelist.component.ts
+++ kv-angularcli-componentdemo/src/app/employee/employeelist.component.ts
@@ -8,14 +8,33 @@ import { Component } from '@angular/core';
 
 export class EmployeeListComponent {
     employees: any[];
+
+    selectedEmployeeCountRadioButton:string='All';
     constructor() {
         this.employees = [
             { Id: 101, firstName: 'Aone', lastName: 'xyzone', Gender: 'Male', Age: 29, Salary: 62000.5233 },
             { Id: 102, firstName: 'Atwo', lastName: 'xyztwo', Gender: 'Male', Age: 37, Salary: 159393.33 },
             { Id: 103, firstName: 'Athree ', lastName: 'xyzthree', Gender: 'Male', Age: 38, Salary: 172330.3993 },
             { Id: 104, firstName: 'Afour ', lastName: 'xyzfour', Gender: 'Female', Age: 31, Salary: 92330.3993 },
-            { Id: 105, firstName: 'Afive', lastName: 'xyzfive', Gender: 'Female', Age: 34, Salary: 112330.3993 }
+            { Id: 105, firstName: 'Afive', lastName: 'xyzfive', Gender: 'Female', Age: 34, Salary: 112330.3993 },
+            { Id: 105, firstName: 'Asix', lastName: 'xyzsix', Gender: 'Male', Age: 32, Salary: 92330.3993 }
         ];
+    }
+
+    onEmployeeCountRadioButtonChanged(selectedRadioButtonValue:string):void{
+        this.selectedEmployeeCountRadioButton = selectedRadioButtonValue;
+    }
+
+    GetTotalEmployeeCount():number{
+        return this.employees.length;
+    }
+
+    GetTotalMaleEmployeeCount():number{
+        return this.employees.filter(x=>x.Gender==='Male').length;
+    }
+
+    GetTotalFemaleEmployeeCount():number{
+        return this.employees.filter(x=>x.Gender==='Female').length;
     }
     // RefreshEmployees(): void {
     //     this.employees = [
